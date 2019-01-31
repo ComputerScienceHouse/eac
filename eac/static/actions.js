33fb437f53c6070f4a46dc626e04f2a3be885e05
Steven Mirabito <stevenmirabito@gmail.com>
2019-01-31 02:47:16 -0500

    Rewrite JS, beautify UI

---
+++ eac/static/actions.js
@@ -0,0 +1,28 @@
+(function () {
+    const reload = () => window.location.reload();
+    const controls = document.querySelectorAll("button[data-service]");
+
+    for (const control of controls) {
+        const serviceName = control.dataset.service;
+        const endpoint = window.location + serviceName;
+        const unlink = control.dataset.action === "unlink";
+
+        control.addEventListener('click', () => {
+            for (const control of controls) {
+                control.disabled = true;
+            }
+
+            if (unlink) {
+                fetch(endpoint, {
+                    method: "DELETE",
+                    credentials: "same-origin"
+                })
+                    .then(reload)
+                    .catch(reload);
+            } else {
+                const popup = window.open(endpoint, serviceName, "height=800,width=600");
+                popup.addEventListener("message", reload);
+            }
+        });
+    }
+}());
